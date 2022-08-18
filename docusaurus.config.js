b31d8ae0f9ac3a1975c5379ce238f03b5e0a6913
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-08-18 04:39:07 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Adball Tech',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Subscribe Now',
   url: 'https://www.adball.tech',
   baseUrl: '/',
   onBrokenLinks: 'throw',
